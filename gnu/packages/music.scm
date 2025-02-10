3564a99a9864e549a96a709435454d40ea84ce4d
宋文武 <iyzsong@member.fsf.org>
2025-02-10 12:11:14 +0800

    gnu: distrho-ports: Fix build with newer freetype.
    * gnu/packages/music.scm (distrho-ports) [arguments]: Add -fpermissive to
    cpp_args.
    
    Change-Id: I65e9f54449dd770c5b1c712d1d7f99034152600f

--- gnu/packages/music.scm
+++ gnu/packages/music.scm
@@ -6924,6 +6924,8 @@ ones.")
       (arguments
        (list
         #:tests? #f                     ;no test target
+        #:configure-flags
+        #~'("-Dcpp_args=-fpermissive") ;from `unsigned char*' to `char*'
         #:phases
         #~(modify-phases %standard-phases
             (add-after 'unpack 'patch-juce-fonts
