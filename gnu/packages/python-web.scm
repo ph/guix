bffc56d662e70eba4cb084cb24b13fb9aade0b0d
Nicolas Graves <ngraves@ngraves.fr>
2025-02-25 17:42:53 +0100

    gnu: Remove python-dictpath.
    * gnu/packages/python-xyz.scm (python-dictpath): Remove variable.
    * gnu/packages/python-web.scm (python-openapi-core)[propagated-inputs]:
    Remove unecessary python-dictpath.

--- gnu/packages/python-web.scm
+++ gnu/packages/python-web.scm
@@ -3182,7 +3182,6 @@ compliance with the specification.")
     (propagated-inputs
      (list python-attrs
            python-aiohttp
-           python-dictpath
            python-isodate
            python-jsonschema
            python-lazy-object-proxy
